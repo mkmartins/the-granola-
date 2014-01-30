496460af684ab81ee15aaeb2b026e5f65c5801ac
Marcos Martins <marcos.martins2307@gmail.com>
2014-01-30 13:38:56 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Granola::Application.config.secret_token = 'af2bdabca8295accb3b93ab18c129a254165543fa09981de043de4b9d65e38876cae0e4fb54d663c625694520744eb8ec55ba4ae7cfc13fa2b3aeedf5fe15546'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Granola::Application.config.session_store :cookie_store, key: '_granola_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Granola::Application.config.session_store :active_record_store
